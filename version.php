f8331584b1b4c37a365c7e7ac24213c22735fdd5
Aaron Wells <aaronw@catalyst.net.nz>
2015-01-06 11:57:36 +1300

    Update version.php

--- version.php
+++ version.php
@@ -27,5 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2014082001;
 $plugin->requires  = 2012062500;
 $plugin->component = 'assignsubmission_mahara';
-$plugin->maturity  = MATURITY_STABLE
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '1.0-mdl2.6'; // Plugin version 1.0, for Moodle 2.6+
